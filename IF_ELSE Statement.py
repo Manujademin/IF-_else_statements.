3dcb991e0dbfc6eeb57228deb2f604e7c20661cd
Manujademin <137299721+Manujademin@users.noreply.github.com>
2023-07-30 03:14:01 -0700

    Add files via upload

---
+++ IF_ELSE Statement.py
@@ -0,0 +1,9 @@
+#lets code a system that ditect people who are banned from a store  
+name = input("Enter you're name? \n")
+if name == "tom" or name =="Tom":
+    
+    print("Tom you are not allowed to be here.")
+
+else:
+    print("hello "+name +" welcome to our shop")
+
